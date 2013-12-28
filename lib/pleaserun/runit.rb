df0d3c4d7113be1f60c0ff3be02212444a3d9df7
Jordan Sissel <jls@semicomplete.com>
2013-12-28 12:46:34 -0800

    Refactor runit support to use attributes.
    Also modify template to experiment with building command line settings
    in the shell.

--- lib/pleaserun/runit.rb
+++ lib/pleaserun/runit.rb
@@ -1,35 +1,16 @@
 require 'pleaserun/namespace'
 require 'pleaserun/base'
 
-class PleaseRun::RunIt < PleaseRun::Base
-  attr_accessor :service_dir, :run_dir
-
-  def initialize(target_platform)
-    super(target_platform)
-
-    case target_platform
-    when /^(debian|ubuntu)/
-      @service_dir = '/etc/service'
-      @run_dir     = '/etc/sv'
-    end
-  end
-
-  def template_path
-    return File.join("templates", platform)
-  end
-
-  def install_actions
-    svdir  = safe_filename("{{ service_dir }}/{{ name }}")
-    rundir = safe_filename("{{ run_dir }}/{{ name }}")
-    return Enumerator::Generator.new do |enum|
-      enum.yield "/bin/ln -s #{svdir} #{rundir}"
-    end
+class PleaseRun::Runit < PleaseRun::Base
+  attribute :service_path, "The path runit service directory",
+            :default => "/service" do |path|
+    insist { path }.is_a?(String)
   end
 
   def files
     return Enumerator::Generator.new do |enum|
-      enum.yield [ safe_filename("{{ service_dir }}/{{ name }}/run"), render_template('run') ]
-      enum.yield [ safe_filename("{{ service_dir }}/{{ name}}/log/run"), render_template('log') ]
+      enum.yield [ safe_filename("{{ service_path }}/{{ name }}/run"), render_template('run') ]
+      enum.yield [ safe_filename("{{ service_path }}/{{ name}}/log/run"), render_template('log') ]
     end
   end
 end
