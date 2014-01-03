8ed0bf77faefddbbb4878f0d51ba19b1ff77cf15
Jordan Sissel <jls@semicomplete.com>
2014-01-03 04:54:07 +0000

    - remove duplicate comments

--- lib/pleaserun/sysv.rb
+++ lib/pleaserun/sysv.rb
@@ -2,17 +2,6 @@ require "pleaserun/base"
 require "pleaserun/namespace"
 
 class PleaseRun::SYSV < PleaseRun::Base
-  # Returns which an enumerable will yield [path, content] for any files
-  # necessary to implement this runner.
-  #
-  # - path: a string path where the file should be put
-  # - content: a text blob content for the file
-  #
-  # Example usage:
-  #
-  # run.files do |path, content|
-  #   File.write(path, content)
-  # end
   def files
     return Enumerator::Generator.new do |out|
       out.yield [ safe_filename("/etc/init.d/{{ name }}"), render_template("init.d") ]
