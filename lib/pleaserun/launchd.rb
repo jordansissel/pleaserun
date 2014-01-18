56e174bde1047dbbef71c03b0f285bf5206f72d6
Jordan Sissel <jls@semicomplete.com>
2014-01-17 22:09:11 -0800

    - move LaunchDaemons plist path computation to a method. - don't run 'launchctl start' as an install action. - improve spacing alignment of generated plist.

--- lib/pleaserun/launchd.rb
+++ lib/pleaserun/launchd.rb
@@ -2,24 +2,28 @@ require "pleaserun/base"
 require "pleaserun/namespace"
 
 class PleaseRun::LaunchD < PleaseRun::Base
+  def daemons_path
+    return safe_filename("/Library/LaunchDaemons/{{ name }}.plist")
+  end
+
   def files
     return Enumerator::Generator.new do |out|
       # Quoting launchctl(1):      
       #    "/Library/LaunchDaemons         System wide daemons provided by the administrator."
-      out.yield [ safe_filename("/Library/LaunchDaemons/{{ name }}"), render_template("program.plist") ]
+      out.yield [ daemons_path, render_template("program.plist") ]
     end
   end
 
   def install_actions
     return [ 
-      "launchctl load #{safe_filename("/Library/LaunchDaemons/{{ name }}")}",
-      render("launchctl start {{name}}"),
+      "launchctl load #{daemons_path}",
+      #render("launchctl start {{name}}"),
     ]
   end
 
   def xml_args
     return if args.nil?
-    return args.collect { |a| "<string>#{a}</string>" }.join("")
+    return args.collect { |a| "<string>#{a}</string>" }.join("\n        ")
   end # def xml_args
 end
 
