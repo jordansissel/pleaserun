d1705e4345d652f8c3ba56822bd652425719052c
Jordan Sissel <jls@semicomplete.com>
2014-01-18 23:26:29 -0800

    - Add sysv deployment tests

--- spec/pleaserun/sysv_spec.rb
+++ spec/pleaserun/sysv_spec.rb
@@ -22,4 +22,67 @@ describe PleaseRun::SYSV do
       insist { files }.include?("/etc/default/fancypants")
     end
   end
+
+  context "deployment" do
+    partytime = superuser?
+    it "cannot be attempted", :if => !partytime do
+      pending("we are not the superuser") unless superuser?
+    end
+
+    context "as the super user", :if => partytime do
+      subject { PleaseRun::SYSV.new("ubuntu-12.04") }
+
+      before do
+        subject.name = "example"
+        subject.user = "root"
+        subject.program = "/bin/sh"
+        subject.args = [ "-c", "echo hello world; sleep 5" ]
+
+        subject.files.each do |path, content|
+          File.write(path, content)
+        end
+        subject.install_actions.each do |command|
+          system(command)
+          raise "Command failed: #{command}" unless $?.success?
+        end
+      end
+
+      after do
+        system_quiet("/etc/init.d/#{subject.name} stop")
+        subject.files.each do |path, content|
+          File.unlink(path) if File.exist?(path)
+        end
+
+        # TODO(sissel): Remove the logs, too.
+        #log = "/var/log/example.log"
+        #File.unlink(log) if File.exist?(log)
+      end
+
+      it "should install" do
+        insist { File }.exist?("/etc/init.d/#{subject.name}")
+      end
+
+      it "should start" do
+        # Status should fail before starting
+        system_quiet("/etc/init.d/#{subject.name} status")
+        reject { $? }.success?
+
+        system_quiet("/etc/init.d/#{subject.name} start")
+
+        system("sh -x /etc/init.d/#{subject.name} status")
+        insist { $? }.success?
+      end
+
+      it "should stop" do
+        system_quiet("/etc/init.d/#{subject.name} start")
+        insist { $? }.success?
+
+        system_quiet("/etc/init.d/#{subject.name} stop")
+        insist { $? }.success?
+
+        system_quiet("/etc/init.d/#{subject.name} status")
+        reject { $? }.success?
+      end
+    end
+  end # real tests
 end
