0753e8f6194eb603ac67c773e816e17e71a68306
Jordan Sissel <jls@semicomplete.com>
2014-01-18 21:37:49 -0800

    Add appropriate /etc/init.d/ script for upstart trickery.

--- lib/pleaserun/upstart.rb
+++ lib/pleaserun/upstart.rb
@@ -4,7 +4,7 @@ class PleaseRun::Upstart < PleaseRun::Base
   def files
     return Enumerator::Generator.new do |out|
       out.yield [ safe_filename("/etc/init/{{ name }}.conf"), render_template("init.conf") ]
-      out.yield [ safe_filename("/etc/init.d/{{ name }}"), render_template("init.conf") ]
+      out.yield [ safe_filename("/etc/init.d/{{ name }}"), render_template("init.d.sh") ]
       # also needs to create a link from /etc/init.d/{{name}} to /lib/init/upstart-job
       #out.yield ["/etc/default/#{name}", Mustache.render(File.read(template("default")), self)]
     end
