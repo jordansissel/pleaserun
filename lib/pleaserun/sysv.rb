b87fcc05ca44ee8d35234c85180a53aeedd70a18
Jordan Sissel <jls@semicomplete.com>
2014-01-21 07:14:03 +0000

    Add file modes to file generator

--- lib/pleaserun/sysv.rb
+++ lib/pleaserun/sysv.rb
@@ -4,7 +4,7 @@ require "pleaserun/namespace"
 class PleaseRun::SYSV < PleaseRun::Base
   def files
     return Enumerator::Generator.new do |out|
-      out.yield [ safe_filename("/etc/init.d/{{ name }}"), render_template("init.d") ]
+      out.yield [ safe_filename("/etc/init.d/{{ name }}"), render_template("init.d"), 0755 ]
       out.yield [ safe_filename("/etc/default/{{ name }}"), render_template("default") ]
     end
   end
