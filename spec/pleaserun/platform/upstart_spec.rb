8edb9bfb271f9d097dcfde720c36f14646e9ed4a
Jordan Sissel <jls@semicomplete.com>
2014-02-02 23:11:52 -0800

    Add 'pre-start' support to upstart w/ tests.

--- spec/pleaserun/platform/upstart_spec.rb
+++ spec/pleaserun/platform/upstart_spec.rb
@@ -2,6 +2,11 @@ require "testenv"
 require "pleaserun/platform/upstart"
 
 describe PleaseRun::Platform::Upstart do
+  let(:start) { "initctl start #{subject.name}" }
+  let(:stop) { "initctl stop #{subject.name}" }
+  let(:status) { "initctl status #{subject.name} | egrep -v '#{subject.name} stop/'" }
+  let(:restart) { "initctl restart #{subject.name}" }
+
   it "inherits correctly" do
     insist { PleaseRun::Platform::Upstart.ancestors }.include?(PleaseRun::Platform::Base)
   end
@@ -53,14 +58,7 @@ describe PleaseRun::Platform::Upstart do
         subject.user = "root"
         subject.program = "/bin/sh"
         subject.args = [ "-c", "echo hello world; sleep 5" ]
-
-        subject.files.each do |path, content|
-          File.write(path, content)
-        end
-        subject.install_actions.each do |command|
-          system(command)
-          raise "Command failed: #{command}" unless $?.success?
-        end
+        activate(subject)
       end
 
       after do
@@ -74,33 +72,46 @@ describe PleaseRun::Platform::Upstart do
         File.unlink(log) if File.exist?(log)
       end
 
-      it "should install" do
-        system_quiet("initctl status #{subject.name}")
-        insist { $? }.success?
-      end
+      #it "should install" do
+        #system("initctl status #{subject.name}")
+        #status_stopped
+      #end
 
       it "should start" do
-        system_quiet("initctl start #{subject.name}")
-        insist { $? }.success?
+        starts
 
         # Starting an already-started job will fail
-        system_quiet("initctl start #{subject.name}")
-        reject { $? }.success?
+        insist { starts }.fails
 
-        system_quiet("initctl status #{subject.name}")
-        insist { $? }.success?
+        status_running
       end
 
       it "should stop" do
-        system_quiet("initctl start #{subject.name}")
-        insist { $? }.success?
-        system_quiet("initctl stop #{subject.name}")
-        insist { $? }.success?
-
-        # Stopping an already-stopped job will fail
-        system_quiet("initctl stop #{subject.name}")
-        reject { $? }.success?
+        starts
+        stops
       end
-    end
+
+      context "with failing prestart" do
+        before do
+          subject.prestart = "false"
+          activate(subject)
+        end
+
+        it "should fail to start" do
+          insist { starts }.fails
+        end
+      end
+
+      context "with successful prestart" do
+        before do
+          subject.prestart = "true"
+          activate(subject)
+        end
+
+        it "should start" do
+          starts
+        end
+      end
+    end # as the super user
   end # real tests
 end
