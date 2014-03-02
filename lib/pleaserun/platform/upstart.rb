34eccea50f0c3cf5878e2510d281e8ce98e8beab
Jordan Sissel <jls@semicomplete.com>
2014-03-02 01:18:35 +0000

    The /etc/init.d/ shim needs to be executable.

--- lib/pleaserun/platform/upstart.rb
+++ lib/pleaserun/platform/upstart.rb
@@ -4,9 +4,7 @@ class PleaseRun::Platform::Upstart < PleaseRun::Platform::Base
   def files
     return Enumerator::Generator.new do |out|
       out.yield [ safe_filename("/etc/init/{{ name }}.conf"), render_template("init.conf") ]
-      out.yield [ safe_filename("/etc/init.d/{{ name }}"), render_template("init.d.sh") ]
-      # also needs to create a link from /etc/init.d/{{name}} to /lib/init/upstart-job
-      #out.yield ["/etc/default/#{name}", Mustache.render(File.read(template("default")), self)]
+      out.yield [ safe_filename("/etc/init.d/{{ name }}"), render_template("init.d.sh"), 0755 ]
     end
   end
 end
