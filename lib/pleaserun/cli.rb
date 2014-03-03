d8c59dc143f1069c0ac62fa3b45ed0af624c7e15
Jordan Sissel <jls@semicomplete.com>
2014-03-03 08:01:43 +0000

    Try to improve error reporting
    - Also add some doc comments to the code
    - fix a bug in target_version setting

--- lib/pleaserun/cli.rb
+++ lib/pleaserun/cli.rb
@@ -10,27 +10,37 @@ class PleaseRun::CLI < Clamp::Command
   class Error < StandardError; end
   class ConfigurationError < Error; end
   class PlatformLoadError < Error; end
+  class FileWritingFailure < Error; end
 
   option ["-p", "--platform"], "PLATFORM", "The name of the platform to target, such as sysv, upstart, etc"
-  option ["-v", "--version"], "VERSION", "The version of the platform to target, such as 'lsb-3.1' for sysv or '1.5' for upstart"
+  option ["-v", "--version"], "VERSION", "The version of the platform to target, such as 'lsb-3.1' for sysv or '1.5' for upstart",
+    :default => "default", :attribute_name => :target_version
 
-  option "--install", :flag, "Install it"
+  option "--install", :flag, "Install the service"
 
   PleaseRun::Platform::Base.attributes.each do |facet|
     # Skip program and args
-    next if [:program, :args].include?(facet.name)
+    next if [:program, :args, :target_version].include?(facet.name)
 
     option "--#{facet.name}", facet.name.to_s.upcase, facet.description,
       :attribute_name => facet.name
   end
+  
+  # TODO(sissel): Make options based on other platforms
 
   base = PleaseRun::Platform::Base
+
+  # Load the 'program' attribute from the Base class and use it as the first
+  # cli parameter.
   program = base.attributes.find { |f| f.name == :program }
   raise "Something is wrong; Base missing 'program' attribute" if program.nil?
   parameter "PROGRAM", program.description, :attribute_name => program.name
 
+  # Load the 'args' attribute from the Base class
+  # and use it as the remaining arguments setting
   args = base.attributes.find { |f| f.name == :args }
   raise "Something is wrong; Base missing 'args' attribute" if program.nil?
+
   parameter "[ARGS] ...", args.description, :attribute_name => args.name
 
   def execute
@@ -38,8 +48,8 @@ class PleaseRun::CLI < Clamp::Command
 
     if platform.nil?
       require "pleaserun/detector"
-      self.platform, self.version = PleaseRun::Detector.detect
-      @logger.warn("No platform selected. Autodetecting one.", :platform => platform, :version => self.version)
+      self.platform, self.target_version = PleaseRun::Detector.detect
+      @logger.warn("No platform selected. Autodetecting...", :platform => platform, :version => target_version)
     end
     platform_klass = load_platform(platform)
 
@@ -48,13 +58,15 @@ class PleaseRun::CLI < Clamp::Command
       @logger.warn("No name given, setting reasonable default", :name => self.name)
     end
 
-    runner = platform_klass.new(version)
+    # Load the platform implementation
+    runner = platform_klass.new(target_version)
+
     platform_klass.all_attributes.each do |facet|
       # Get the value of this attribute
-      # This is akin to simply calling `someattribute` method.
+      # The idea here is to translate CLI options to runner settings
       value = send(facet.name)
       next if value.nil?
-      @logger.debug("Setting runner", :attribute => facet.name, :value => value)
+      @logger.debug("Setting runner attribute", :name => facet.name, :value => value)
 
       # Set the value in the runner we've selected
       # This is akin to `obj.someattribute = value`
@@ -62,29 +74,59 @@ class PleaseRun::CLI < Clamp::Command
     end
 
     tmp = Stud::Temporary.directory
+    errors = []
     runner.files.each do |path, content, perms|
-      if install?
-        fullpath = path
-      else
-        fullpath = File.join(tmp, path)
-      end
-      @logger.log("Writing file", :destination => fullpath, :mode => perms)
-      FileUtils.mkdir_p(File.dirname(fullpath))
-      File.write(fullpath, content)
-      File.chmod(perms, fullpath) if perms
+      #perms ||= (0666 ^ File.umask)
+      fullpath = install? ? path : File.join(tmp, path)
+      success = write(fullpath, content, perms)
+      errors << fullpath unless success
     end
+
+    if errors.any?
+      raise FileWritingFailure, "Errors occurred while writing files"
+    end
+
+    # TODO(sissel): Refactor this to be less lines of code or put into methods.
+    if runner.install_actions.any?
+      if install?
+        runner.install_actions.each do |action|
+          @logger.info("Running install action", :action => action)
+          system(action)
+        end
+      else
+        path = File.join(tmp, "install_actions.sh")
+        @logger.log("Writing install actions. You will want to run this script to properly activate your service on the target host", :path => path)
+        fd = File.new(path, "w")
+        runner.install_actions.each do |action|
+          fd.puts(action)
+        end
+        fd.close
+      end
+    end # if runner.install_actions.any?
 
     return 0
   rescue Error => e
-    puts "An error occurred: #{e}"
+    @logger.error("An error occurred: #{e}")
     return 1
+  end # def execute
+
+  def write(fullpath, content, perms)
+    @logger.log("Writing file", :destination => fullpath)
+    FileUtils.mkdir_p(File.dirname(fullpath))
+    File.write(fullpath, content)
+    @logger.debug("Setting permissions", :destination => fullpath, :perms => perms)
+    File.chmod(perms, fullpath) if perms
+    return true
+  rescue Errno::EACCES
+    @logger.error("Access denied in writing a file. Maybe we need to be root?", :path => fullpath)
+    return false
   end
 
   def setup_logger
     @logger = Cabin::Channel.new
     @logger.subscribe(STDOUT)
     @logger.level = :warn
-  end
+  end # def setup_logger
 
   def load_platform(v)
     @logger.debug("Loading platform", :platform => v)
@@ -99,5 +141,5 @@ class PleaseRun::CLI < Clamp::Command
     return PleaseRun::Platform.const_get(const)
   rescue LoadError => e
     raise PlatformLoadError, "Failed to find or load platform '#{v}'. This could be a typo or a bug. If it helps, the error is: #{e}"
-  end
-end
+  end # def load_platform
+end # class PleaseRun::CLI
