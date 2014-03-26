1040298c804edae8b11a667d5903a5ca4c6b270c
Jordan Sissel <jls@semicomplete.com>
2014-03-25 22:38:19 -0700

    Call it init.sh

--- lib/pleaserun/platform/sysv.rb
+++ lib/pleaserun/platform/sysv.rb
@@ -7,7 +7,7 @@ require "pleaserun/namespace"
 class PleaseRun::Platform::SYSV < PleaseRun::Platform::Base
   def files
     return Enumerator::Generator.new do |out|
-      out.yield(safe_filename("/etc/init.d/{{ name }}"), render_template("init.d"), 0755)
+      out.yield(safe_filename("/etc/init.d/{{ name }}"), render_template("init.sh"), 0755)
       out.yield(safe_filename("/etc/default/{{ name }}"), render_template("default"))
     end
   end
