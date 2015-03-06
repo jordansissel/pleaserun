b64514066957cee18b437c2679e2210c5a0cbadf
Jordan Sissel <jls@semicomplete.com>
2015-03-05 18:50:23 -0800

    Capture stdout and stderr to files with respective names.
    Should address #76

--- templates/sysv/lsb-3.1/init.sh
+++ templates/sysv/lsb-3.1/init.sh
@@ -57,7 +57,7 @@ start() {
     {{{ulimit_shell}}}
     cd \"$chdir\"
     exec \"$program\" $args
-  " >> {{{ sysv_log }}}.log 2>> {{{ sysv_log }}}.err &
+  " >> {{{ sysv_log }}}.stdout 2>> {{{ sysv_log }}}.stderr &
 
   # Generate the pidfile from here. If we instead made the forked process
   # generate it there will be a race condition between the pidfile writing
