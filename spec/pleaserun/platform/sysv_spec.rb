be46c8f70cf010b0703af1195ff54a5362d7374c
Jordan Sissel <jls@semicomplete.com>
2015-04-29 10:30:55 -0700

    Clarify sysv test skipping when /etc/init.d can't be written to

--- spec/pleaserun/platform/sysv_spec.rb
+++ spec/pleaserun/platform/sysv_spec.rb
@@ -9,7 +9,7 @@ describe PleaseRun::Platform::SYSV do
   context "deployment", :sysv => true do
     etc_initd_writable = File.lstat("/etc/init.d").writable? rescue false
     if !etc_initd_writable
-      it "cannot write to /etc/init.d, so there's no deployment test to do. You'll need to run this as root, probably. Be careful, if you do run this as root."
+      it "cannot write to /etc/init.d, so there's no deployment test to do. To run these tests, you'll need to run this as root, and preferrably in a vm or other temporary system"
     else
       it_behaves_like PleaseRun::Platform do
         let(:skip) { "Cannot write to /etc/init.d" } unless etc_initd_writable
