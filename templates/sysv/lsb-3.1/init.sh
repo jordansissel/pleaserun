450de7eeb088302ce912e8d24ef6e1f880d936a3
Luke Morfitt <luke.morfitt@piksel.com>
2015-07-20 13:59:27 +0100

    Attempt to allow RHEL5(and derivatives) which use a old chroot version to run

--- templates/sysv/lsb-3.1/init.sh
+++ templates/sysv/lsb-3.1/init.sh
@@ -60,7 +60,7 @@ start() {
 
   # Run the program!
   {{#nice}}nice -n "$nice" \{{/nice}}
-  chroot --userspec "$user":"$group" "$chroot" sh -c "
+  sh -c "
     {{{ulimit_shell}}}
     cd \"$chdir\"
     exec \"$program\" $args
